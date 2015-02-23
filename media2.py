39d41aed40f186a46014e905073e68b073942cde
gwdavis <computer@davisfamily.com>
2015-02-23 14:09:23 -0500

    Change tab spacing
    Changed tab spacing using Sublime

--- media2.py
+++ media2.py
@@ -6,17 +6,17 @@ class Movie():
    """This class provide a way to store and show video information"""
 
 def __init__(self):
-        self.title = ""
-        self.storyline = ""
-        self.poster_image_url = ""
-        self.genre = ""
-        self.duration = ""
-        self.rating = ""
-        self.release_date = ""
-        self.trailer_youtube_id = ""
+    self.title = ""
+    self.storyline = ""
+    self.poster_image_url = ""
+    self.genre = ""
+    self.duration = ""
+    self.rating = ""
+    self.release_date = ""
+    self.trailer_youtube_id = ""
 
 def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
+    webbrowser.open(self.trailer_youtube_url)
 
         
     
